8f7a435323567c8e90538e33ccad3dbe975c9594
asimard1 <as.simard@outlook.com>
2026-07-13 21:51:15 -0400

    A few changes for my versions

--- Assets/Script/Gameplay/BackgroundManager.cs
+++ Assets/Script/Gameplay/BackgroundManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using Cinemachine;
+using Unity.Cinemachine;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using UniHumanoid;
