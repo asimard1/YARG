bf1a1fb5bdda717c9f7b5a419c065ff264788e77
asimard1 <as.simard@outlook.com>
2026-07-13 22:54:38 -0400

    Package fixes

--- Assets/Script/Gameplay/BackgroundManager.cs
+++ Assets/Script/Gameplay/BackgroundManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using Unity.Cinemachine;
+using Cinemachine;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using UniHumanoid;
